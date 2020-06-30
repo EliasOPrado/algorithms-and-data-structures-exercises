2a5879553515f05f77ac25b670afdbe659dd7d05
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-06-30 03:12:20 +0100

    added more comments

--- algorithms_and_data_structures_exercises/python_exercises/quick_sort.py
+++ algorithms_and_data_structures_exercises/python_exercises/quick_sort.py
@@ -1,24 +1,29 @@
 
+# sequence: [0, 9, 5, 4, 3, 2, 4, 5, 6, 7, 8, 9, 1]
 def quick_sort(sequence):
-    #gets the int value of the length of the sequence
+    #gets the int value of the length of the sequence.
     lenght = len(sequence)
-    #if the length int value is 0 return the array/sequence
+    #if the length int value is 0 return the array/sequence.
     if lenght <= 1:
         return sequence
     else:
-        #takes the last item from the sequence
+        #takes the last item from the sequence BEFORE the loop.
         pivot = sequence.pop()
+        #e.g: pivot = [1]  from the sequence list.
 
     items_greater = []
     items_lower = []
-    #loops from the lower item to the higher from thre list
+
     for item in sequence:
-        #since the item is looping 1,2...
-        # if item is greater than pivot which is already removed from the last
+        #loops from the lower item to the higher from thre list
+        # since the item is looping 1stnumb, 2ndnum, 3rdnumb... e.g: [0, 9, 5, 4, 3, 2, 4, 5, 6, 7, 8, 9]
         if item > pivot:
+            # if item is greater than pivot which is already removed from the end of the sequence... see last comment above.
             items_greater.append(item)
+            # the item will be added/appended to the [] items_greater list/array
         else:
             items_lower.append(item)
+            # otherwise it will be added
     return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
 
 print(quick_sort([0,9,5,4,3,2,4,5,6,7,8,9,1]))
@@ -26,9 +31,9 @@ print(quick_sort([0,9,5,4,3,2,4,5,6,7,8,9,1]))
 """
 [items_lower][pivot][items_greater]
 
-[0,9,5,4,3,2,4,5,6,7,8,9,1]
+[0, 9, 5, 4, 3, 2, 4, 5, 6, 7, 8, 9, 1]
 
-1- [0,][1][9,5,4,3,2,5,6,7,8,9]
+1- [0, 1, 9, 5, 4, 3, 2, 5, 6, 7, 8, 9]
 
 2- [0]
 
